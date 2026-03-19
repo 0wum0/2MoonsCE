4d685a77b64c0c90e64cb27d512ac28016182230
0wum0 <florian0engelhardt@gmail.com>
2026-03-19 09:49:17 +0100

    fix: refactor shipyard.js to use native integers instead of DecimalNumber and improve code clarity
    Replaced DecimalNumber with native parseInt for Amount tracking since shipyard quantities are always integers. Consolidated variable declarations, simplified time calculation logic, improved loop conditions (length-1 to length, == to ===), and cleaned up select element manipulation in ShipyardList. Moved clearInterval before page refresh to ensure timer stops before navigation.

--- scripts/game/shipyard.js
+++ scripts/game/shipyard.js
@@ -1,55 +1,50 @@
-var v  			= new Date();
+var v  = new Date();
+var Shipyard, Amount, hanger_id, ShipyardInterval;
 
 function ShipyardInit() {
-	Shipyard		= data.Queue;
-	Amount			= new DecimalNumber(Shipyard[0][1],0);
-	hanger_id		= data.b_hangar_id_plus;
+	Shipyard  = data.Queue;
+	Amount    = parseInt(Shipyard[0][1], 10);
+	hanger_id = data.b_hangar_id_plus;
 	$('#timeleft').text(data.pretty_time_b_hangar);
 	ShipyardList();
 	BuildlistShipyard();
-	ShipyardInterval	= window.setInterval(BuildlistShipyard, 1000);
+	ShipyardInterval = window.setInterval(BuildlistShipyard, 1000);
 }
 
 function BuildlistShipyard() {
 	var n = new Date();
-	var s = Shipyard[0][2] - hanger_id - Math.round((n.getTime() - v.getTime()) / 1000);
-	var s = Math.round(s);
-	var m = 0;
-	var h = 0;
+	var s = Math.round(Shipyard[0][2] - hanger_id - (n.getTime() - v.getTime()) / 1000);
 	if (s <= 0) {
-		Amount.sub('1');
+		Amount -= 1;
 		$('#val_'+Shipyard[0][3]).text(function(i, old){
 			return ' ('+bd_available+NumberGetHumanReadable(parseInt(old.replace(/.* (.*)\)/, '$1').replace(/\./g, ''))+1)+')';
-		})
-		if (Amount.toString() == '0') {
+		});
+		if (Amount <= 0) {
 			Shipyard.shift();
-			if (Shipyard.length == 0) {
+			if (Shipyard.length === 0) {
 				$("#bx").html(Ready);
 				document.getElementById('auftr').options[0] = new Option(Ready);
-				if (typeof SmAjax !== 'undefined') { SmAjax.refreshPageContent(); } else { document.location.href = document.location.href; }
 				window.clearInterval(ShipyardInterval);
+				if (typeof SmAjax !== 'undefined') { SmAjax.refreshPageContent(); } else { document.location.href = document.location.href; }
 				return;
 			}
-			Amount = Amount.reset(Shipyard[0][1]);
+			Amount = parseInt(Shipyard[0][1], 10);
 			ShipyardList();
 		} else {
-			document.getElementById('auftr').options[0].innerHTML	= Amount.toString() + " " + Shipyard[0][0] + " " + bd_operating;
+			document.getElementById('auftr').options[0].innerHTML = Amount + " " + Shipyard[0][0] + " " + bd_operating;
 		}
 		hanger_id = 0;
 		v = new Date();
 		s = 0;
 	}
-	$("#bx").html(Shipyard[0][0]+" "+GetRestTimeFormat(s));
+	$("#bx").html(Shipyard[0][0] + " " + GetRestTimeFormat(s));
 }
 
 function ShipyardList() {
-	while (document.getElementById('auftr').length > 0)
-		document.getElementById('auftr').options[document.getElementById('auftr').length - 1] = null;
-
-	for (iv = 0; iv <= Shipyard.length - 1; iv++ ) {
-		if(iv == 0)
-			document.getElementById('auftr').options[iv] = new Option(Amount.toString()+ " " + Shipyard[iv][0] + " " + bd_operating, iv);
-		else
-			document.getElementById('auftr').options[iv] = new Option(Shipyard[iv][1]+ " " + Shipyard[iv][0] + " " + bd_operating, iv);
+	var sel = document.getElementById('auftr');
+	while (sel.options.length > 0) sel.options[sel.options.length - 1] = null;
+	for (var iv = 0; iv < Shipyard.length; iv++) {
+		var label = (iv === 0 ? Amount : Shipyard[iv][1]) + " " + Shipyard[iv][0] + " " + bd_operating;
+		sel.options[iv] = new Option(label, iv);
 	}
 }
